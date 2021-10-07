dfe9aff26e8a1709c9473ba6b9c7157ae68593ea
Artem Krivonozhko <artemkrivonozhko@gmail.com>
2021-10-07 18:35:48 +0300

    Move exclude to application.rb

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module DangerRails
-  class Application < Rails::Application
+  class Application < Rails::Application # rubocop:disable Style/Documentation
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
